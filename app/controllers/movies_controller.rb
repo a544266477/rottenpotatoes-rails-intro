cca1dbaff72d3d1965fbb820c6991aebec212b95
Guobin Liang <a544266477@berkeley.edu>
2022-10-05 03:46:02 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,26 +8,9 @@ class MoviesController < ApplicationController
   
     def index
       #@movies = Movie.all
-      sort = params[:sort] || session[:sort]
-      case sort
-      when 'title'
-        ordering,@title_header = {:title => :asc}, 'hilite'
-      when 'release_date'
-        ordering,@date_header = {:release_date => :asc}, 'hilite'
-      end
       @all_ratings = Movie.all_ratings
-      @selected_ratings = params[:ratings] || session[:ratings] || {}
-  
-      if @selected_ratings == {}
-        @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
-      end
-  
-      if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
-        session[:sort] = sort
-        session[:ratings] = @selected_ratings
-        redirect_to :sort => sort, :ratings => @selected_ratings and return
-      end
-      @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+      @ratings_to_show = check
+      @movies = Movie.where(rating: @ratings_to_show)
     end
   
     def new
@@ -69,14 +52,13 @@ class MoviesController < ApplicationController
       return ratings
     end
 
-
     private
     # Making "internal" methods private is not required, but is a common practice.
     # This helps make clear which methods respond to requests, and which ones do not.
     def movie_params
       params.require(:movie).permit(:title, :rating, :description, :release_date)
     end
-  
+
     def check
       if params[:ratings]
         params[:ratings].keys
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,16 @@
 class Movie < ActiveRecord::Base
   def self.all_ratings
-    %w(G PG PG-13 NC-17 R)
+    ['G','PG','PG-13','R']
+  end
+
+  def self.with_ratings(ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    # if ratings_list is nil, retrieve ALL movies
+    if ratings_list.nil?
+      Movie.all
+    else
+      Movie.where(rating: ratings_list)
+    end
   end
 end
